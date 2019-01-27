785d3bcec6a481663cf93e7a4bf405ee5dc34589
Oskarovsky <oskar.slyk@gmail.com>
2019-01-27 10:22:07 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        final String appName = "Mp3Player v0.4";
+        try {
+            Parent parent = (Parent) FXMLLoader.load(getClass().getResource(
+                    "sample.fxml"));
+            Scene scene = new Scene(parent);
+            primaryStage.setTitle(appName);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
