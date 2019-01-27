84af1e61db8b8a4520298ce244f0eefdb82224cb
Oskarovsky <oskar.slyk@gmail.com>
2019-01-27 10:42:15 +0100

    Added event handling

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,10 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,9 +13,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.input.MouseEvent;
 
 public class Controller implements Initializable {
 
@@ -49,7 +52,38 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
+        configureButtons();
+        configureVolume();
     }
+
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Button on the slider has been clicked");
+            }
+        });
+    }
+
+    private void configureButtons() {
+        prevButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Previous track");
+            }
+        });
+        nextButton.setOnAction(x -> System.out.println("Next track"));
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(playButton.isSelected()) {
+                    System.out.println("Play");
+                } else {
+                    System.out.println("Stop");
+                }
+            }
+        });
+    }
+
 
 }
