d4300f9d261d9e1c1ab9ca5c8c79d8b62ef52e03
Oskarovsky <oskar.slyk@gmail.com>
2019-01-28 21:18:28 +0100

    Changed main table

--- src/sample/controller/ContentPaneController.java
+++ src/sample/controller/ContentPaneController.java
@@ -2,18 +2,40 @@ package sample.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.mp3.Mp3Song;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ContentPaneController implements Initializable {
 
+    public static final String TITLE_COLUMN = "Title";
+    public static final String AUTHOR_COLUMN = "Author";
+    public static final String ALBUM_COLUMN = "Album";
+
     @FXML
-    private TableView<?> contentTable;
+    private TableView<Mp3Song> contentTable;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        configureTable();
+    }
 
+    private void configureTable() {
+        TableColumn<Mp3Song, String> titleColumn = new TableColumn<Mp3Song, String>(TITLE_COLUMN);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Mp3Song, String> authorColumn = new TableColumn<Mp3Song, String>(AUTHOR_COLUMN);
+        authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+        TableColumn<Mp3Song, String> albumColumn = new TableColumn<Mp3Song, String>(ALBUM_COLUMN);
+        albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
+
+        contentTable.getColumns().add(titleColumn);
+        contentTable.getColumns().add(authorColumn);
+        contentTable.getColumns().add(albumColumn);
     }
 }
