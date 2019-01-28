eead0d4e56685943aa009f2320cc239520c6a6b5
Oskarovsky <oskar.slyk@gmail.com>
2019-01-28 21:42:25 +0100

    Testing mp3player with .mp3 file

--- src/sample/controller/ContentPaneController.java
+++ src/sample/controller/ContentPaneController.java
@@ -5,8 +5,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import sample.mp3.Mp3Collection;
 import sample.mp3.Mp3Song;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,13 +20,38 @@ public class ContentPaneController implements Initializable {
     public static final String TITLE_COLUMN = "Title";
     public static final String AUTHOR_COLUMN = "Author";
     public static final String ALBUM_COLUMN = "Album";
+    private Mp3Collection mp3Collection;
 
     @FXML
     private TableView<Mp3Song> contentTable;
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
+    public void initialize(URL location, ResourceBundle resources) {
         configureTable();
+        configureTestMp3();
+    }
+
+    private void configureTestMp3() {
+        mp3Collection = new Mp3Collection();
+        contentTable.setItems(mp3Collection.getSongList());
+
+        Mp3Song mp3Song = createMp3SongFromPath("test.mp3");
+        mp3Collection.addSong(mp3Song);
+    }
+
+    private Mp3Song createMp3SongFromPath(String filePath) {
+        File file = new File(filePath);
+        Mp3Song result = new Mp3Song();
+        try {
+            MP3File mp3File = new MP3File(file);
+            result.setFilePath(file.getAbsolutePath());
+            result.setTitle(mp3File.getID3v2Tag().getSongTitle());
+            result.setAuthor(mp3File.getID3v2Tag().getLeadArtist());
+            result.setAlbum(mp3File.getID3v2Tag().getAlbumTitle());
+        } catch (IOException | TagException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     private void configureTable() {
